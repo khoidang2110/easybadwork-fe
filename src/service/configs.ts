0ce057f40a01aeb47edda0163a970b4241dc9b69
khoidang2110 <khoidang2110@gmail.com>
2024-06-12 15:13:46 +0700

    khoi.

--- src/service/configs.ts
+++ src/service/configs.ts
@@ -2,5 +2,6 @@ import axios from "axios";
 
 export const axiosClient = axios.create({
     // baseURL: 'http://localhost:8081',
-    baseURL:'http://14.225.218.217:8081',
+   // baseURL:'http://14.225.218.217:8081',
+   baseURL:'https://khoi.easybadwork.com'
   });
