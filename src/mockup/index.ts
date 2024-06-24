895d72b6af4a0cb34d3581af8b5ee50145137ace
khoidang2110 <khoidang2110@gmail.com>
2024-06-24 09:55:38 +0700

    stock8

--- src/mockup/index.ts
+++ src/mockup/index.ts
@@ -1,26 +1,3 @@
 import { IProduct } from '@/interfaces/product';
 
-export const products: IProduct[] = [
-  {
-    product_id: '1',
-    name: 'cam',
-    price_vnd: 555,
-    price_usd: 55,
-    desc_vi: 'string',
-    desc_en: 'string',
-    category: 'string',
-    image: [],
-    stock: [],
-  },
-  {
-    product_id: '1',
-    name: 'cam',
-    price_vnd: 1,
-    price_usd: 11,
-    desc_vi: 'string',
-    desc_en: 'string',
-    category: 'string',
-    image: [],
-    stock: [],
-  },
-];
+
